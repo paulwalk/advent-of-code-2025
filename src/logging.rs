345c7c2582c9e33c9e3b543e2f0e2dacac243f24
Paul Walk <paul@paulwalk.net>
2026-04-09 09:20:04 +0100

    changed logging format

--- src/logging.rs
+++ src/logging.rs
@@ -1,21 +1,8 @@
-use flexi_logger::{style, DeferredNow, LogSpecification};
-use log::Record;
+use flexi_logger::{colored_detailed_format};
 
-pub fn configure_flexi_logger(log_spec: String) -> Result<(), Box<dyn std::error::Error>> {
-    let spec = LogSpecification::parse(log_spec).unwrap();
-    flexi_logger::Logger::with(spec).log_to_stdout().set_palette("1;5;32;3;-".parse()?).format(custom_logging_format).start()?;
+pub fn configure_flexi_logger(log_level: String) -> Result<(), Box<dyn std::error::Error>> {
+    flexi_logger::Logger::try_with_str(log_level)?.log_to_stdout().set_palette("1;5;32;3;-".parse()?).format(colored_detailed_format).start()?;
     log::debug!("Debug level logging is enabled");
     Ok(())
 }
 
-pub fn custom_logging_format(w: &mut dyn std::io::Write, _now: &mut DeferredNow, record: &Record) -> Result<(), std::io::Error> {
-    let level = record.level();
-    write!(
-        w,
-        "{}: {}:{} ",
-        style(level).paint(level.to_string()),
-        style(level).paint(record.file().unwrap_or("<unnamed>")),
-        style(level).paint(record.line().unwrap_or(0).to_string()),
-    )?;
-    write!(w, "{}", record.args())
-}
